d97f632abbc70c53502ce1c10a17b28d9e5c9704
EARLPOGI14 <160315515+EARLPOGI14@users.noreply.github.com>
2024-02-17 17:50:15 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+﻿function respondYes() {
+    document.getElementById("response").innerHTML = "Wow, cant wait lods!";
+}
+
+function respondNo() {
+    document.getElementById("response").innerHTML = "Awittt, libre ko na sana";
+}
